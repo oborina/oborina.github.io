cab5d6578f60083a7e5e86685a07a4a4bfc0bf6f
Anastasiya Oborina <yasnastya2010@gmail.com>
2019-06-04 23:57:25 +0300

    Commit portfolio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+var link = document.querySelector(".button-modal");
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--close');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
