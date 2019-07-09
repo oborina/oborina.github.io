bfeb92fdf84271b2ecc3634ff38b3b9dde2a921e
Anastasiya Oborina <yasnastya2010@gmail.com>
2019-07-09 20:31:48 +0300

    boot

---
+++ js/jq.js
@@ -0,0 +1,7 @@
+/*$('.page-header__wrapper-header').scroll(function () {
+  $('this').css('background-color', 'red');
+});*/
+$(document).ready(function () {
+  $('page-header__wrapper').css('background-color', 'red');
+});
+
